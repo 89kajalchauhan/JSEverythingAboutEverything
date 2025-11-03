ccf64c3961c26cbc65e167d04f1d53761295b2e8
Kajal <kajal123@gmail.com>
2025-11-03 15:56:11 +0530

    Initial commit: added HTML and JS files

---
+++ Script.js
@@ -0,0 +1,9 @@
+
+let sel = document.querySelector("select");
+let device = document.querySelector("#device");
+
+
+sel.addEventListener("change",function(dets){
+  
+  device.textContent = `$`
+});
