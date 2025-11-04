b3e5515e7d276afaf15745a281b2db489e57bbee
Kajal <kajal123@gmail.com>
2025-11-04 16:36:29 +0530

    updated some code

--- Script.js
+++ Script.js
@@ -4,20 +4,62 @@
 //sel.addEventListener("change", function (dets) {
 //  device.textContent = `${dets.target.value} Device Selected`;}):
 
-let h1 = document.querySelector("h1");
+//let h1 = document.querySelector("h1");
 
-window.addEventListener("keydown", function(dets){
- if(dets.key === " "){
-  h1.textContent = "SPC";
+//window.addEventListener("keydown", function(dets){
+// if(dets.key === " "){
+  //h1.textContent = "SPC";
 
- } else{
-  h1.textContent = dets.key;
- }
+// } else{
+//  h1.textContent = dets.key;
+// }
 
 
 
 //h1.textContent = dets.key;
 
+//});
+
+
+let Button = document.querySelector("#Button");
+ let fileinp = document.querySelector("#fileinp");
+
+Button.addEventListener("click", function(){
+   fileinp.click();
+     
+
 });
+
+fileinp.addEventListener("change", function(dets){
+  
+    const file = dets.target.files[0],
+    if (file) {
+     Button.textContent = file.name; 
+    }
+   }
+  
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
