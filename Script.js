56cc2f8bae4c14c4940e7c8343da5671bfcac0d7
Kajal <kajal123@gmail.com>
2025-11-09 00:15:55 +0530

    Updated project code

--- Script.js
+++ Script.js
@@ -1,6 +1,13 @@
+ let form = document.querySelector("form");
+ let input = document.querySelectorAll("input");
 
 
 
+form.addEventListener("submit", function(dets){
+    
+    dets.preventDefault();
+    
+})
 
 
 
